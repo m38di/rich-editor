fba519be3eda6fd71fdb885d8488bca1d209362d
Mehdi <m38diam@gmail.com>
2026-07-28 07:20:15 -0700

    Update ivIcons.tsx

--- src/components/ivIcons.tsx
+++ src/components/ivIcons.tsx
@@ -117,7 +117,6 @@ export function Iv({ name, size = 22, className }: IvProps & { name: string }) {
       height={size}
       viewBox={icon.vb}
       className={className}
-      style={{ color: typeof color !== "undefined" ? color : "white" }}
       aria-hidden
       dangerouslySetInnerHTML={{ __html: icon.inner }}
     />
