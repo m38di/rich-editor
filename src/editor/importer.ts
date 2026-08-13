a7910b6ecb3358a2e20f8b2448ccb1a5eec83eb2
Mehdi <m38diam@gmail.com>
2026-08-13 00:46:17 -0700

    Update importer.ts

--- src/editor/importer.ts
+++ src/editor/importer.ts
@@ -1,8 +1,18 @@
-import { DOMParser as PMDOMParser, Node as PMNode } from 'prosemirror-model'
+import { DOMParser as ProseMirrorDOMParser, Node as PMNode } from 'prosemirror-model'
 import { schema } from './schema'
 
-export function parseHtml(html: string): PMNode {
+const MAX_HTML_SIZE = 5 * 1024 * 1024
+
+export function importHtml(html: string): PMNode {
+  if (new Blob([html]).size > MAX_HTML_SIZE) {
+    throw new Error('HTML file is larger than 5 MB')
+  }
+
   const dom = new DOMParser().parseFromString(html, 'text/html')
 
-  return PMDOMParser.fromSchema(schema).parse(dom.body)
+  if (dom.querySelector('parsererror')) {
+    throw new Error('Invalid HTML')
+  }
+
+  return ProseMirrorDOMParser.fromSchema(schema).parse(dom.body)
 }
